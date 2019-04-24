316b77701885e85e5ca0e07e65159829c8d8138f
R <740767@cognizant.com>
2019-04-24 12:43:26 +0530

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ import 'bootstrap';
 })
 export class AppComponent {
   title = 'Wedding';
+  time: number;
+  constructor() {
+    var startDate = new Date();
+    var endDate = new Date('05/17/2019');
+    this.time = (endDate.getTime() - startDate.getTime()) / 1000;
+  }
 }
